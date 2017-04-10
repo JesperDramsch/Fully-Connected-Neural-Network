5afed167936526ec6dffbd0581d573e4b13c282f
Jesper Dramsch <jesper@dramsch.net>
2017-04-10 18:56:10 +0200

    Dropout

--- myNN.m
+++ myNN.m
@@ -4,13 +4,18 @@ clc;
 format compact
 
 addpath('..')
+data = getDataNN(3,1000,.1,1);
 
-iterations = 10;
+iterations = 100;
+n_layers = 3; %number of layers
 neurons = 4; %+bias
 m = 2; %number of inputs
 y = 2; %number of outputs
+eta = 1e-2;
 type = 'relu' % sigmoid, tanh or relu
 mode = 'mini-batch' % batch, mini-batch or stochastic
+dropout = true; % Dropout flag
+
 
 figure
 plotNN(m,neurons,y,n_layers)
@@ -19,16 +24,17 @@ sizedata = size(data,1);
 unimod = m^-.5;
 
 w = cell([n_layers-1, 1]);
-
 w(1) = {-unimod+2*unimod.*(rand([neurons, m+1]))};
 for layers = 2:n_layers-2
     w(layers) = {-unimod+2*unimod.*(rand(neurons,neurons+1))};
 end
 w(n_layers-1) = {-unimod+2*unimod.*(rand([y, neurons+1]))};
 
+
+
 switch mode
     case 'mini-batch'
-        batchsize = 10;
+        batchsize = 25;
     case 'batch'
     	batchsize = sizedata;
 	case 'stochastic'
@@ -41,12 +47,18 @@ end
 figure
 for epoch = 1:iterations
     out = zeros(size(data,1),y);
+    i_data = randperm(sizedata);
     for batch=1:floor(sizedata/batchsize);
         points=(batch-1)*batchsize+1:(batch)*batchsize;
         a=cell([n_layers-1, 1]);
         z=cell([n_layers, 1]);
         delta=z;
-        z{1}= [data(points,1:2) ones(batchsize,1)];
+        if dropout
+            z{1}= [data(i_data(points),1:2).*(round(rand(size(data(i_data(points),1:2))).^.5)) ones(batchsize,1)];
+        else
+            z{1}= [data(i_data(points),1:2) ones(batchsize,1)];
+        end
+        
 
 %% Forward propagate 
         for layer = 1:n_layers-1
@@ -57,7 +69,7 @@ for epoch = 1:iterations
         z{n_layers}=softmax(a{end}')';
 
 %% Evaluate Delta k for output layer
-        delta{n_layers} = z{n_layers}-data(points,3:4);
+        delta{n_layers} = z{n_layers}-data(i_data(points),3:4);
 
 %% Backpropagate Delta j for hidden layers
         for hidden = n_layers-1:-1:2
@@ -69,7 +81,7 @@ for epoch = 1:iterations
             w{layer} = w{layer}-eta*deriv_E_w{layer};
         end
         
-        out(points,:) = z{end};
+        out(i_data(points),:) = z{end};
     end
 %% Error calculation
     
@@ -78,18 +90,30 @@ for epoch = 1:iterations
     hold on
 end
 
+%% Plot
+figure
+id = find(out(:,1)>.5);
+plot(data(id,1), data(id,2), 'b.', 'MarkerSize', 20);
+hold on
+id = find(out(:,2)>.5);
+plot(data(id,1), data(id,2), 'r.', 'MarkerSize', 20);
+id = find(round(out(:,1)) ~= data(:,3));
+plot(data(id,1), data(id,2), 'g.', 'MarkerSize', 5);
+id = find(round(out(:,2)) ~= data(:,4));
+plot(data(id,1), data(id,2), 'g.', 'MarkerSize', 5);
+legend('Class 1', 'Class 2', 'Missclassified')
+
+%% Test
 a=cell([n_layers-1, 1]);
 z=cell([n_layers, 1]);
 delta=z;
-        z{1}= [data(:,1:2) ones(sizedata,1)];
-
-%% Forward propagate 
+z{1}= [data(:,1:2) ones(sizedata,1)];
 for layer = 1:n_layers-1
     a{layer} = z{layer}*w{layer}';
 	z{layer+1}=[activator(a{layer},type) ones(sizedata,1)];
 end
-
 z{n_layers}=softmax(a{end}')';
+out=z{end};
 
 display(sprintf('The total missclassification in the final run is %0.2f%%.',100-sum(round(out(:,2)) == data(:,4))/sizedata*100))
 
